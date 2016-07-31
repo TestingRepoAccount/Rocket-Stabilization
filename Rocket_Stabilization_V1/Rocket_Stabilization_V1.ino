4a796211af625ec5e8cca4b9891560a88f0c7c9c
Jeremy Hall <Coldreactor64@gmail.com>
2016-07-31 10:57:44 -0500

    Add Version 1 of rocket stabilization code

--- Rocket_Stabilization_V1/Rocket_Stabilization_V1.ino
+++ Rocket_Stabilization_V1/Rocket_Stabilization_V1.ino
@@ -1,18 +1,19 @@
+/*
+ * Author: Jeremy Hall
+ * Credits to Kris Winer at Sparkfun Electronics
+ * for Mahony and Madgwick Filtering functions.
+ */
+#include <Servo.h>
 #include<Wire.h>
 //#include <I2C.h>
-//#include <Adafruit_Sensor.h>
-//#include <Adafruit_HMC5883_U.h>
 
-const int MPU=0x68;  // I2C address of the MPU-6050
-const int Mag = 0x1E;
-float AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ,MgX,MgY,MgZ;
-float lastUpdate = 0;    // used to calculate integration interval
-float Now = 0;           // used to calculate integration interval
 //#define OutputAccel
-#define OutputGyro
+//#define OutputGyro
 //#define OutputMag
-//#define OutputYawPitchRoll
 //#define OutputQuat
+//#define OutputYawPitchRoll
+#define OutputCalibration
+
 
 #define GyroMeasError PI * (40.0f / 180.0f)       // gyroscope measurement error in rads/s (shown as 3 deg/s)
 #define GyroMeasDrift PI * (0.0f / 180.0f)      // gyroscope measurement drift in rad/s/s (shown as 0.0 deg/s/s)
@@ -20,35 +21,57 @@ float Now = 0;           // used to calculate integration interval
 #define zeta sqrt(3.0f / 4.0f) * GyroMeasDrift   // compute zeta, the other free parameter in the Madgwick scheme usually set to a small or zero value
 #define Kp 2.0f * 5.0f // these are the free parameters in the Mahony filter and fusion scheme, Kp for proportional feedback, Ki for integral
 #define Ki 0.0f
-float yaw;
-float pitch;
-float roll;
+
+float lastUpdate = 0;    // used to calculate integration interval
+float Now = 0;           // used to calculate integration interval
 float deltat = 0.0f;        // integration interval for both filter schemes
 float q[4] = {1.0f, 0.0f, 0.0f, 0.0f};    // vector to hold quaternion
 float eInt[3] = {0.0f, 0.0f, 0.0f};       // vector to hold integral error for Mahony method
-/* Assign a unique ID to this sensor at the same time */
-//Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(12345);
+
+
+const int MPU=0x68;  // I2C address of the MPU-6050
+const int Mag = 0x1E; //I2C address of the HMC5833L
+float AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ,MgX,MgY,MgZ; //Raw Data
+float yaw, pitch, roll;
+double pitchopp, pitchdiff, rollopp, rolldiff;// Variabls for finding oppisite values based on the current position
+
+int rolloffset = -8; //servo 1
+int pitchoffset = 8;  //servo 2
+int rolloffset1 = 5; //servo 3
+int pitchoffset1 = 0; //servo 4
+Servo pitchserv, pitchserv1, rollserv, rollserv1;
+float pitchOutput, rollOutput;
 
 void setup(){
   Wire.begin();
   setupSensors();
-  Serial.begin(115200);
-//  mag.begin();
- // Wire.beginTransmission(MPU);
-//  Wire.write(0x6B);  // PWR_MGMT_1 register
-//  Wire.write(0);     // set to zero (wakes up the MPU-6050)
-//  Wire.endTransmission(true);
+  Serial.begin(9600);
+  rollserv.attach(3);//Servo 1
+  pitchserv.attach(5);// Servo 2
+  rollserv1.attach(6);// Servo 3
+  pitchserv1.attach(9);// Servo 4
 }
 
 void loop(){
-
+//Get Sensor Updates
 readGyroAccel();
 readMag();
-  Now = micros();
-  deltat = ((Now - lastUpdate)/1000000.0f); // set integration time by time elapsed since last filter update
-
+//Get Time Data then use a Mahony Filter
+Now = micros();
+deltat = ((Now - lastUpdate)/1000000.0f); // set integration time by time elapsed since last filter update
 MahonyQuaternionUpdate(AcX, AcY, AcZ, GyX*PI/180.0f, GyY*PI/180.0f, GyZ*PI/180.0f, MgX, MgY, MgZ);
+//MadgwickQuaternionUpdate(AcX, AcY, AcZ, GyX*PI/180.0f, GyY*PI/180.0f, GyZ*PI/180.0f, MgX, MgY, MgZ);
 lastUpdate = micros();
+
+yaw   = atan2(2.0f * (q[1] * q[2] + q[0] * q[3]), q[0] * q[0] + q[1] * q[1] - q[2] * q[2] - q[3] * q[3]);   //convert to yaw, pitch and roll
+pitch = -asin(2.0f * (q[1] * q[3] - q[0] * q[2]));
+roll  = atan2(2.0f * (q[0] * q[1] + q[2] * q[3]), q[0] * q[0] - q[1] * q[1] - q[2] * q[2] + q[3] * q[3]);
+pitch *= 180.0f / PI;
+yaw   *= 180.0f / PI; 
+roll  *= 180.0f / PI;
+// After Converting to Yaw, Pitch and Roll compute what is needed to control the rocket
+ComputePitch();
+ComputeRoll();
 
 #ifdef OutputAccel
 Serial.print("\t");
@@ -91,13 +114,6 @@ Serial.println("\t");
 #endif
 
 #ifdef OutputYawPitchRoll
-yaw   = atan2(2.0f * (q[1] * q[2] + q[0] * q[3]), q[0] * q[0] + q[1] * q[1] - q[2] * q[2] - q[3] * q[3]);   
-pitch = -asin(2.0f * (q[1] * q[3] - q[0] * q[2]));
-roll  = atan2(2.0f * (q[0] * q[1] + q[2] * q[3]), q[0] * q[0] - q[1] * q[1] - q[2] * q[2] + q[3] * q[3]);
-pitch *= 180.0f / PI;
-yaw   *= 180.0f / PI; 
-yaw   -= 13.8; // Declination at Danville, California is 13 degrees 48 minutes and 47 seconds on 2014-04-04
-roll  *= 180.0f / PI;
 Serial.print(yaw);
 Serial.print("\t");
 Serial.print(pitch);
@@ -105,8 +121,19 @@ Serial.print("\t");
 Serial.println(roll);
 #endif
 
-//Serial.println(deltat);
-//Serial.print("filter rate = "); Serial.println(1.0f/deltat, 1);
+#ifdef OutputCalibration
+Serial.print(pitch);
+Serial.print("\t");
+Serial.print(roll);
+Serial.print("\t");
+Serial.print(pitchOutput);
+Serial.print("\t");
+Serial.println(rollOutput);
+#endif
+
+
+Serial.print(deltat);
+Serial.println("filter rate = "); Serial.println(1.0f/deltat, 1);
 }
 
 void setupSensors() {
@@ -120,21 +147,41 @@ void setupSensors() {
   Wire.endTransmission(true);
   
   Wire.beginTransmission(MPU);
-  Wire.write(0x6A);  // PWR_MGMT_1 register
-  Wire.write(0);     // set to zero (wakes up the MPU-6050)
+  Wire.write(0x6A);  //Set Half of what is needed for I2CMaster Bypass
+  Wire.write(0);     
   Wire.endTransmission(true);
   
   Wire.beginTransmission(MPU);
-  Wire.write(0x37);  // PWR_MGMT_1 register
-  Wire.write(0x02);     // set to zero (wakes up the MPU-6050)
+  Wire.write(0x37);  //Set last part of I2CMaster Bypass
+  Wire.write(0x02);     
   Wire.endTransmission(true);
 
   Wire.beginTransmission(Mag); //open communication with HMC5883
   Wire.write(0x02); //select mode register
   Wire.write(0x00); //continuous measurement mode
   Wire.endTransmission();
-  
 }
+
+void ComputeRoll(){
+rollOutput = map(roll, -30, 30, 70, 110);
+/*calculate oppisite angle for the opp servo*/
+rolldiff = 90 - rollOutput;
+rollopp = 90 + rolldiff;
+rollserv.write(rollOutput + rolloffset);//Servo 1
+rollserv1.write(rollopp + rolloffset1);//Servo 4
+}
+
+void ComputePitch(){
+
+pitchOutput = map(pitch, -30, 30, 70, 110);
+/*calculate oppisite angle for the opp servo*/
+pitchdiff = 90 - pitchOutput;
+pitchopp = 90 + pitchdiff;
+pitchserv.write(pitchOutput + pitchoffset);//Original Servo
+pitchserv1.write(pitchopp + pitchoffset1);//Op. Servo
+
+}
+
 
 void readGyroAccel(){
   //Start Recieving Data
@@ -149,13 +196,15 @@ void readGyroAccel(){
   GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
   GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
   GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
-//Offset the input data
+/* This is offsetting your data, If you find your data to not be centered correctly
+ *  find the offsets for your data and add or subract them here 
+ */
   AcX -= 800.0f;//FYI I just found what seemed to be the best 
   AcZ += 1449.0f;//and got me the most stable data with the noise centered around the 0
   GyX += 250.0f;//Your values may be different
   GyY -= 30.0f;
   GyZ += 40.0f;
-//divide it by scaling factor as per datasheet
+/*Division by the scaling factor as per the datasheet for the MPU6050 */
   AcX /= 16384.0f;
   AcY /= 16384.0f;
   AcZ /= 16384.0f;
@@ -166,13 +215,6 @@ void readGyroAccel(){
 
 
 void readMag(){
-/*sensors_event_t event; 
-mag.getEvent(&event);
-
-MgX = event.magnetic.x;
-MgY = event.magnetic.y;
-MgZ = event.magnetic.z;
-*/
   Wire.beginTransmission(Mag);//Address of HMC5883L Magnetometer
   Wire.write(0x3);  // starting with register 0x3B (ACCEL_XOUT_H)
   Wire.endTransmission(false);
@@ -184,7 +226,6 @@ MgZ = event.magnetic.z;
   MgY *= .01;
   MgZ *= .01;
 }
-
 
 void MahonyQuaternionUpdate(float ax, float ay, float az, float gx, float gy, float gz, float mx, float my, float mz)
    {
